7a8eacecb2e8bd4017e31fb6b0d734f04ee22014
Bráian Pereira <braianlfp@gmail.com>
2022-11-08 23:45:36 -0300

    Atualização aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
+import React from "react";
 import config from "../config.json"
 import styled from "styled-components";
 import {CSSReset} from "../src/components/CSSReset";
-import Menu from "../src/components/menu";
+import Menu from "../src/components/Menu";
 import {StyledTimeline} from "../src/components/TimeLine";
 import Favoritos from "../src/components/Favoritos";
 
 function HomePage() {
+	const [valorDoFiltro, setValorDoFiltro] = React.useState('')
 	return (
 		<>
 			<CSSReset />
@@ -14,9 +16,9 @@ function HomePage() {
 				flexDirection: "column",
 				flex: 1
 			}}>
-				<Menu />
+				<Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
 				<Header />
-				<TimeLine  playlists={config.playlists}/>
+				<TimeLine searchValue={valorDoFiltro} playlists={config.playlists}/>
 				<Favoritos />
 			</div>
 		</>
@@ -32,27 +34,24 @@ const StyledHeader = styled.div`
 	  border-radius: 50%;
 	}
   .user-info {
-    margin-top: 50px;
 	display: flex;
 	align-items: center;
 	width: 100%;
 	padding: 16px 32px;
 	gap: 16px;
   }
-  .banner {
-    width: 100%;
-    height: 45vh;
-    object-fit: cover;
-	
-  }
 `
+
+const StyledBanner = styled.div `{
+  background-image: url(${({bg}) => bg});
+  //background-image: url("${config.banner}");
+  height: 230px; 
+}`
 
 function Header() {
 	return (
 		<StyledHeader>
-			<section>
-				<img className={'banner'} src={config.banner} alt=""/>
-			</section>
+			<StyledBanner bg={config.banner} />
 			<section className={'user-info'}>
 				<img className={'perfil-img'} src={`https://github.com/${config.github}.png`} alt=""/>
 				<div>
@@ -64,29 +63,35 @@ function Header() {
 	)
 }
 
-function TimeLine(props) {
+function TimeLine({searchValue, ...props}) {
 	// console.log("Dentro do componente", props)
 	const playlistNames = Object.keys(props.playlists)
 	//Statement
 	//Retorno por expressão
 	return (
 		<StyledTimeline>
-			{playlistNames.map((playlistName, index) => {
+			{playlistNames.map((playlistName) => {
 				const videos = props.playlists[playlistName]
 				return (
-					<section key={index}>
+					<section key={playlistName}>
 						<h2>{playlistName}</h2>
 						<div>
-							{videos.map((video, videoIndex) => {
-								return (
-									<a key={videoIndex} href={video.uri}>
-										<img src={video.thumb} alt=""/>
-										<span>
-											{video.title}
-										</span>
-									</a>
-									)
+							{videos
+								.filter((video) => {
+									const titleNormalized = video.title.toLowerCase()
+									const searchValueNormalized = searchValue.toLowerCase()
+									return titleNormalized.includes(searchValueNormalized)
 								})
+								.map((video, videoIndex) => {
+									return (
+										<a key={videoIndex} href={video.uri}>
+											<img src={video.thumb} alt=""/>
+											<span>
+												{video.title}
+											</span>
+										</a>
+										)
+									})
 							}
 						</div>
 					</section>
